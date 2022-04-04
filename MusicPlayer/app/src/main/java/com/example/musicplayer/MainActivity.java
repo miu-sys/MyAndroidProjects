e685c46bddc2d0211fe8b6cf4a7d2604dc852fda
miu-sys <ylu46385@gmail.com>
2022-04-04 19:37:38 +0800

    Added sliding progress bar

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -11,9 +11,15 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,8 +35,10 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     //控件
-    private TextView tv_play;//音乐播放按钮
-    private TextView tv_pause;//音乐暂停播放按钮
+    private Button bt_play,bt_pause,bt_continue,bt_quit;//播放、暂停、继续播放、退出按钮
+    private static SeekBar pb_music;//音乐播放器进度条
+    private static TextView tv_progress,tv_total;//音乐播放当前进度、音乐总时间
+
 
     //对象
     MusicService.MyBinder myBinder;
@@ -45,28 +53,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         //初始化控件
-        tv_play = (TextView) findViewById(R.id.tv_play);
-        tv_pause = (TextView) findViewById(R.id.tv_pause);
+        bt_play =  findViewById(R.id.bt_play);
+        bt_pause =  findViewById(R.id.bt_pause);
+        bt_continue =  findViewById(R.id.bt_continue);
+        bt_quit = findViewById(R.id.bt_quit);
+
+        pb_music=(SeekBar)findViewById((R.id.sb));
+
+        tv_progress = findViewById(R.id.tv_progress);
+        tv_total = findViewById(R.id.tv_total);
 
         //设置按钮监听
-        tv_play.setOnClickListener(this);
-        tv_pause.setOnClickListener(this);
+        bt_play.setOnClickListener(this);
+        bt_pause.setOnClickListener(this);
+        bt_continue.setOnClickListener(this);
+        bt_quit.setOnClickListener(this);
 
         //播放音乐
         myConn = new MyConn();
         Intent intent = new Intent(this, MusicService.class);
         bindService(intent, myConn, BIND_AUTO_CREATE);
 
+        //播放进度条
+        pb_music.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if(b){
+                    myBinder.jumpTo(i);//跳转到进度条
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                myBinder.pause();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                myBinder.continuePlay();
+            }
+        });
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.tv_play://点击播放按钮
-                myBinder.playMusic();
+            case R.id.bt_play://点击播放按钮
+                myBinder.play();
                 break;
-            case R.id.tv_pause://点击暂停按钮
+            case R.id.bt_pause://点击暂停按钮
                 myBinder.pause();
+                break;
+            case R.id.bt_continue://点击继续播放按钮
+                myBinder.continuePlay();
+                break;
+            case R.id.bt_quit://点击退出按钮
+                finish();
                 break;
             default:
                 break;
@@ -93,4 +136,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onDestroy();
     }
 
+    //获取从service传递过来的消息
+    public static Handler handler=new Handler(Looper.myLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Bundle bundle = msg.getData();//获取从子线程发送过来的音乐播放器
+            int duration = bundle.getInt("duration");//歌曲总时长
+            int currentPosition = bundle.getInt("currentPosition");//当前播放进度
+
+            pb_music.setMax(duration);//设置进度条播放总时长
+            pb_music.setProgress(currentPosition);//设置进度条当前播放时长
+
+            //格式化获取的时间
+            String totalTime = msToMinSec(duration);
+            String currentTime = msToMinSec(currentPosition);
+
+            //将格式化的时间设置到UI控件
+            tv_progress.setText(currentTime);
+            tv_total.setText(totalTime);
+
+        }
+    };
+
+    //将获取到的时间格式化为00:00
+    public static String msToMinSec(int ms){
+        int sec = ms / 1000;
+        int min = sec / 60;
+        sec -= min*60;
+        return  String.format("%02d:%02d",min,sec);
+
+    }
 }
--- MusicPlayer/app/src/main/java/com/example/musicplayer/MusicService.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MusicService.java
@@ -5,37 +5,53 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Message;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MusicService extends Service {
 
-    public MediaPlayer mediaPlayer;
+    public MediaPlayer mediaPlayer;//多媒体对象
+    public Timer timer;//时钟对象
 
     @Override
     public void onCreate() {
         mediaPlayer = new MediaPlayer();
-        mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.ericaceae);//设置音乐播放路径
+        mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.qianqiu);//设置音乐播放路径
         super.onCreate();
     }
 
     //自定义的类，用于向Activity提供方法的接口
     class MyBinder extends Binder {
         //播放音乐
-        public void playMusic() {
-            if(getCurrentProgress()!=0){//如果是暂停后播放，跳转到当前播放进度
-                int position = getCurrentProgress();//获取当前播放进度
-                mediaPlayer.seekTo(position);//跳转到当前播放位置
+        public void play() {
+            if (mediaPlayer != null){
+                mediaPlayer.start();
+                addTimer();
             }
-            mediaPlayer.start();
         }
 
         //暂停播放
         public void pause() {
-            if(mediaPlayer!=null&&mediaPlayer.isPlaying()){
-                mediaPlayer.pause();
-            }else if(mediaPlayer!=null&&(!mediaPlayer.isPlaying())){//再次点击暂停按钮也可以重新播放
-                mediaPlayer.start();
-            }
+            mediaPlayer.pause();
+        }
+
+        //继续播放
+        public void continuePlay() {
+            int position = getCurrentProgress();//获取当前播放进度
+            mediaPlayer.seekTo(position);//跳转到当前播放位置
+            mediaPlayer.start();//开始播放
+        }
+
+        /**
+         *跳转播放进度
+         * @param position 播放进度
+         */
+        void jumpTo(int position){
+            mediaPlayer.seekTo(position);//跳转到当前播放位置
         }
 
     }
@@ -56,6 +72,7 @@ public class MusicService extends Service {
         return new MyBinder();
     }
 
+
     @Override
     public void onDestroy() {
         if(mediaPlayer!=null){
@@ -65,4 +82,29 @@ public class MusicService extends Service {
         }
         super.onDestroy();
     }
+
+    //计时器
+    public void addTimer(){
+        if (timer == null){
+            timer = new Timer();
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    int duration = mediaPlayer.getDuration();//获取歌曲总时长
+                    int currentPosition = mediaPlayer.getCurrentPosition();//获取歌曲当前时长
+                    Message msg = MainActivity.handler.obtainMessage();//创建消息对象
+                    //将需要的音乐数据封装进Bundle中
+                    Bundle bundle = new Bundle();
+                    bundle.putInt("duration",duration);
+                    bundle.putInt("currentPosition",currentPosition);
+                    msg.setData(bundle);
+                    //将消息发送到主线程消息队列
+                    MainActivity.handler.sendMessage(msg);
+                }
+            };
+            //开始计时任务后的5ms，第一次执行task任务，以后每500ms执行一次
+            timer.schedule(task,5,500);
+        }
+    }
+
 }
